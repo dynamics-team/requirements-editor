f305706d44fc139d18ca9d1588142be139d0b7a8
pmeijer <patrik85@isis.vanderbilt.edu>
2014-10-06 14:45:34 -0500

    List requirements and alert on click.

--- public/app.js
+++ public/app.js
@@ -4,17 +4,61 @@ var ReqApp = angular.module('RequirementsApp', [])
     .controller('RequirementsController', function (RequirementsService, $scope) {
         'use strict';
         $scope.dataModel = {
-            all: []
+            requirements: []
         };
         console.log('RequirementsController');
-        $scope.dataModel.all = RequirementsService.listRequirements();
+        $scope.navigateToReq = function (reqName) {
+            RequirementsService.getByName(reqName)
+                .then(function (data) {
+                    console.log(data);
+                    alert(JSON.stringify(data, null, 2));
+                })
+                .catch(function (reason) {
+                    console.log(reason);
+                });
+        }
+        RequirementsService.listAll()
+            .then(function (data) {
+                console.log(data);
+                $scope.dataModel.requirements = data;
+            })
+            .catch(function (reason) {
+                console.log(reason);
+            });
     })
-    .service('RequirementsService', function () {
+    .service('RequirementsService', function ($q, $http) {
         'use strict';
+        var baseUrl = 'http://localhost:8844/'; // TODO: This should probably be configurable
         console.log('RequirementsService');
 
-        this.listRequirements = function () {
-            return ['req1', 'req2'];
+        this.listAll = function () {
+            var deferred = $q.defer(),
+                url = baseUrl + 'requirement/';
+            $http.get(url)
+                .success(function (data, status, headers, config) {
+                    deferred.resolve(data);
+                })
+                .error(function (data, status, headers, config) {
+                    console.error(data);
+                    deferred.reject(status);
+                });
+
+            return deferred.promise;
+        };
+
+        this.getByName = function (name) {
+            var deferred = $q.defer(),
+                url = baseUrl + 'requirement/' + name;
+            $http.get(url)
+                .success(function (data, status, headers, config) {
+                    deferred.resolve(data);
+                })
+                .error(function (data, status, headers, config) {
+                    console.error(data);
+                    deferred.reject(status);
+                });
+
+            return deferred.promise;
         };
     });
 
