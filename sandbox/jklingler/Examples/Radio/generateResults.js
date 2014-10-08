387f40438ec976327bba57df6dbbe69004badae8
James Klingler <jklingler@isis.vanderbilt.edu>
2014-10-08 16:02:14 -0500

    test the refactored generateResults.js

--- sandbox/jklingler/Examples/Radio/generateResults.js
+++ sandbox/jklingler/Examples/Radio/generateResults.js
@@ -3,14 +3,91 @@
  */
 
 'use strict';
+var fs = require('fs');
+var Classes = require('./classes');
+
+function generateResults (requirementsObject, numberResults) {
+    var flatRequirementsMap = {},
+    requirements = new Classes.TopLevelRequirementsGroup(requirementsObject, flatRequirementsMap);
+    return generateTestBenchManifests(flatRequirementsMap, "MyDesign", numberResults);
+}
+
+function generateTestBenchManifests (requirementsMap, seedDesignName, numberResults) {
+    var testbenchManifestGrid = {};
+    var metricMap = {};
+    var name;
+    var numResults = numberResults || 1;
+
+    for (name in requirementsMap) {
+        var requirement = requirementsMap[name];
+        var metricName = requirement.metricName;
+        var metric;
+
+        if (metricMap.hasOwnProperty(metricName)) {
+            metric = metricMap[metricName];
+        } else {
+            metric = new Classes.Metric(requirement)
+            metricMap[metricName] = metric;
+        }
+
+        for (var i=0;i<numResults;i++) {
+            var tbName = requirement.testBench;
+            var dName = seedDesignName + "_" + (i + 1).toString();
+            var uniqueName = tbName + '_' + dName;
+            var tb;
+
+            giveMetricValues(metric, requirement);
+
+            if (testbenchManifestGrid.hasOwnProperty(dName)) {
+                if (testbenchManifestGrid[dName].hasOwnProperty(tbName)) {
+                    tb = testbenchManifestGrid[dName][tbName];
+                } else {
+                    tb = new Classes.TestBenchManifest(tbName, dName);
+                    testbenchManifestGrid[dName][tbName] = tb;
+                }
+            } else {
+                tb = new Classes.TestBenchManifest(tbName, dName);
+                testbenchManifestGrid[dName] = {};
+                testbenchManifestGrid[dName][tbName] = tb;
+            }
+
+            // These two lines are to remove the reference to the "Metric" object.
+            var metricString =JSON.stringify(metric, null, 4);
+            var metricJson = JSON.parse(metricString);
+
+            tb.Metrics.push(metricJson);
+        }
+    }
+
+    var results = [];
+
+    for (dName in testbenchManifestGrid) {
+        var singleResult = [];
+        for (tbName in testbenchManifestGrid[dName]) {
+            singleResult.push(testbenchManifestGrid[dName][tbName]);
+        }
+
+        results.push(singleResult);
+    }
+
+    return results;
+}
+
+function giveMetricValues (metricInstance, requirementInstance) {
+    var difference = Math.abs(requirementInstance.objective - requirementInstance.threshold),
+        average = (requirementInstance.objective + requirementInstance.threshold)/ 2,
+        randomNumber = Math.random(),
+        unitScale = (randomNumber - 0.5);
+
+    metricInstance.Value = average + difference*unitScale;
+}
+
+
 var main = function () {
 
-    var fs = require('fs');
-    var Classes = require('./classes');
-    var args = process.argv.slice(2);
-    var requirementsFilePath;
-    var numberResultsToGenerate;
-    var resultsShouldPass;
+    var args = process.argv.slice(2),
+        requirementsFilePath,
+        numberResultsToGenerate;
 
     if (args[0]) {
         requirementsFilePath = args[0];
@@ -18,107 +95,23 @@ var main = function () {
     if (args[1]) {
         numberResultsToGenerate = args[1];
     }
-    if (args[2]) {
-        if (args[2] === 'true') {
-            resultsShouldPass = true;
-        } else {
-            resultsShouldPass = false;
-        }
-    }
 
-    var giveMetricValues = function (metricInstance, requirementInstance, shouldPass) {
-        var difference = Math.abs(requirementInstance.objective - requirementInstance.threshold);
-        var average = (requirementInstance.objective + requirementInstance.threshold)/2;
-        var randomNumber = Math.random();
-        var unitScale = (randomNumber - 0.5);
-
-        if (shouldPass) {
-            metricInstance.Value = average + difference*unitScale;
-        } else {
-            metricInstance.Value = average + difference*1.5*unitScale;
-        }
-    };
-
-    var generateTestBenchManifests = function (requirementsMap, designName, numberResults) {
-        var testbenchJsons = {};
-        var metricMap = {};
-        var name;
-        var numResults = numberResults || 1;
-
-        for (name in requirementsMap) {
-            var requirement = requirementsMap[name];
-            var metricName = requirement.metricName;
-            var metric;
-
-            if (metricMap.hasOwnProperty(metricName)) {
-                metric = metricMap[metricName];
-            } else {
-                metric = new Classes.Metric(requirement)
-                metricMap[metricName] = metric;
-            }
-
-            for (var i=0;i<numResults;i++) {
-                var tbName = requirement.testBench;
-                //var tbName = requirement.testBench + "_" + (i + 1).toString();
-                var dName = designName + "_" + (i + 1).toString();
-                var uniqueName = tbName + '_' + dName;
-                var tb;
-
-                giveMetricValues(metric, requirement, resultsShouldPass);
-
-                if (testbenchJsons.hasOwnProperty(uniqueName)) {
-                    tb = testbenchJsons[uniqueName];
-                } else {
-                    tb = new Classes.TestBenchManifest(tbName, dName)
-                    testbenchJsons[uniqueName] = tb;
-                }
-
-                // These two lines are to remove the reference to the "Metric" object.
-                var metricString =JSON.stringify(metric, null, 4);
-                var metricJson = JSON.parse(metricString);
-
-                tb.Metrics.push(metricJson);
-            }
+    fs.readFile(requirementsFilePath, 'utf8', function(err, data) {
+        if (err) {
+            console.log("Could not read json file " + filePath);
         }
 
-        for (name in testbenchJsons) {
-            var fName = "testbench_manifest.json";
-            //var dirName = 'z' + Math.random().toString(36).substring(8);
-            fs.mkdir(name, function (err) {
-                if(err) {
-                    console.log(err);
-                }
+        var requirementsObject = JSON.parse(data);
+            //splitPath = requirementsFilePath.split('/'),
+            //title = splitPath[splitPath.length - 1].split('.')[0],
 
-                fs.writeFile(name + '/' + fName, JSON.stringify(testbenchJsons[name], null, 4), function(err) {
-                    if(err) {
-                        console.log(err);
-                    } else {
-                        console.log("The file was saved!");
-                    }
-                });
-            });
-        }
-    };
-
-    var generateResultsFromRequirements = function (filePath) {
-        fs.readFile(filePath, 'utf8', function(err, data) {
-            if (err) {
-                console.log("Could not read json file " + filePath);
-            }
-
-            data = JSON.parse(data);
-            var splitPath = filePath.split('/'),
-                title = splitPath[splitPath.length - 1].split('.')[0],
-                flatRequirementsMap = {},
-                requirements = new Classes.TopLevelRequirementsGroup(title, data, flatRequirementsMap);
-
-            generateTestBenchManifests(flatRequirementsMap, "MyDesign", numberResultsToGenerate);
-        });
-    };
-
-    generateResultsFromRequirements(requirementsFilePath);
+        var result = generateResults(requirementsObject, numberResultsToGenerate);
+        JSON.stringify(result, null, 4);
+    });
 };
 
 if (require.main === module) {
     main();
 }
+
+module.exports.generateResults = generateResults;
