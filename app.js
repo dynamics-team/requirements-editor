edc6c5bf1fc307d89d769f9b00a47db521707fac
James Klingler <jklingler@isis.vanderbilt.edu>
2014-10-09 15:13:48 -0500

    update Radio example to use Float values

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ function start() {
             };
 
             var sampleRequirements = {
-                'Radio Example': 'sandbox/jklingler/Examples/Radio/TopLevelRequirementGroup.json',
+                'Radio Example': 'sandbox/jklingler/Examples/Radio/WalkieTalkieDesign.json',
                 'Container Example': 'sandbox/jklingler/Examples/Container/ContainerDesign.json',
                 'Container Hierarchy Example': 'sandbox/jklingler/Examples/Container/ContainerDesign2.json',
                 'CarShopping Example': 'sandbox/jklingler/Examples/CarBuyer/CarBuyer.json'
