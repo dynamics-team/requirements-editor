0f411c7455b04c60c15cc1a0b67133a47a65242a
pmeijer <patrik85@isis.vanderbilt.edu>
2014-10-09 10:39:53 -0500

    Factor out dependencies.

---
+++ src/client/app/angularRequire.js
@@ -0,0 +1,14 @@
+/**
+ * Created by pmeijer on 10/9/2014.
+ */
+
+// Include the Services
+require('./services/RequirementsService');
+require('./services/SearchService');
+// Include the controllers
+require('./controllers/RequirementsController');
+require('./controllers/RequirementDetailsController');
+require('./controllers/EditRequirementController');
+require('./controllers/NewRequirementController');
+require('./controllers/EditUsersController');
+require('./controllers/SearchController');
