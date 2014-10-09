10bf97acfe056a53b48701c62f2da8e9c7722111
James Klingler <jklingler@isis.vanderbilt.edu>
2014-10-09 16:18:43 -0500

    more plots

--- sandbox/jklingler/scripts/make_plots.py
+++ sandbox/jklingler/scripts/make_plots.py
@@ -13,14 +13,27 @@ def plot_squashing_functions():
     [exponential.append((math.exp(i) - 1.0)/(math.exp(1) - 1.0)) for i in x[251:751]]
     [exponential.append(1) for i in x[751:]]
 
+    inverted_exponential = [1 for i in x[:251]]
+    [inverted_exponential.append(-(math.exp(i) - 1.0)/(math.exp(1) - 1.0) + 1) for i in x[251:751]]
+    [inverted_exponential.append(0) for i in x[751:]]
+
     log = [0 for i in x[:251]]
     [log.append((math.log(i + 1.0))/(math.log(2))) for i in x[251:751]]
     [log.append(1) for i in x[751:]]
 
+    inverted_log = [1 for i in x[:251]]
+    [inverted_log.append(-(math.log(i + 1.0))/(math.log(2)) + 1) for i in x[251:751]]
+    [inverted_log.append(0) for i in x[751:]]
+
+    plt_y_min = -0.1
+    plt_y_max = 1.1
+
     plt.figure()
-    plt.ylim([-0.1, 1.1])
+    plt.ylim([plt_y_min, plt_y_max])
     plt.xlim([-0.5, 1.5])
     plt.plot(x, exponential, label='exp')
+    plt.vlines(0, plt_y_min, plt_y_max, colors='r', label='Threshold')
+    plt.vlines(1, plt_y_min, plt_y_max, colors='g', label='Objective')
     plt.legend(loc=0)
     plt.savefig('exp.svg')
     plt.close()
@@ -29,8 +42,30 @@ def plot_squashing_functions():
     plt.ylim([-0.1, 1.1])
     plt.xlim([-0.5, 1.5])
     plt.plot(x, log, label='log')
+    plt.vlines(0, plt_y_min, plt_y_max, colors='r', label='Threshold')
+    plt.vlines(1, plt_y_min, plt_y_max, colors='g', label='Objective')
     plt.legend(loc=0)
     plt.savefig('log.svg')
+    plt.close()
+
+    plt.figure()
+    plt.ylim([plt_y_min, plt_y_max])
+    plt.xlim([-0.5, 1.5])
+    plt.plot(x, inverted_exponential, label='neg exp')
+    plt.vlines(0, plt_y_min, plt_y_max, colors='g', label='Objective')
+    plt.vlines(1, plt_y_min, plt_y_max, colors='r', label='Threshold')
+    plt.legend(loc=0)
+    plt.savefig('neg_exp.svg')
+    plt.close()
+
+    plt.figure()
+    plt.ylim([-0.1, 1.1])
+    plt.xlim([-0.5, 1.5])
+    plt.plot(x, inverted_log, label='neg log')
+    plt.vlines(0, plt_y_min, plt_y_max, colors='g', label='Objective')
+    plt.vlines(1, plt_y_min, plt_y_max, colors='r', label='Threshold')
+    plt.legend(loc=0)
+    plt.savefig('neg_log.svg')
     plt.close()
 
 if __name__ == "__main__":
